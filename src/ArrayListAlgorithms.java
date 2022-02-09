e70bcb9d54ffe7c96798e064ca6f9bd5ec04550c
BT_1E10_28 <jeffreyc115@nycstudents.net>
2022-02-09 13:56:10 -0500

    Initial commit

---
+++ src/ArrayListAlgorithms.java
@@ -0,0 +1,288 @@
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class ArrayListAlgorithms
+    {
+        /** Returns true if any of the elements in stringList contain
+         *  target as a substring; false otherwise.  Use indexOf instead of contains
+         *  for AP practice! (although in reality, contains is preferred)
+         *
+         *  Does NOT mutate (modify) elements of stringList.
+         *  PRECONDITION: stringList.size() > 0
+         *
+         *  @param stringList  original arraylist of Strings
+         *  @return  true if target is found in any of the strings, false otherwise
+         */
+        public static boolean containsTarget(ArrayList<String> stringList, String target)
+        {
+            for (String str : stringList)
+            {
+                if (str.indexOf(target) != -1) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /** Returns number of elements in intList that are less than the
+         *  average of all elements.
+         *
+         *  Does NOT mutate (modify) elements of intList.
+         *  PRECONDITION: intList.size() > 0
+         *
+         *  @param intList  original arraylist of Integers
+         *  @return  the number of ints in intList that are less than the average
+         */
+        public static int belowAverage(ArrayList<Integer> intList)
+        {
+            int total = 0;
+            for (Integer num : intList)
+            {
+                total += num;
+            }
+            double average = (double) total / intList.size();
+            int counter = 0;
+            for (Integer num: intList)
+            {
+                if (num < average)
+                {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+
+        /** Replaces all words in wordList that end in "s" with the all-uppercase
+         *  version of the word.  For example, "apples" should be replaced with "APPLES".
+         *  Assume all letters of all words in wordList are lowercase initially (no need
+         *  to worry about checking for case or converting first to lowercase)
+         *
+         *  DOES mutate (modify) elements of wordList.
+         *  PRECONDITION: wordList.size() > 0
+         *
+         *  @param wordList  original arraylist of words
+         */
+        public static void replaceWithCaps(ArrayList<String> wordList)
+        {
+            String currentWord = "";
+            for (int i = 0; i < wordList.size(); i++)
+            {
+                currentWord = wordList.get(i);
+                if (currentWord.substring(currentWord.length() - 1, currentWord.length()).equals("s"))
+                {
+                    wordList.set(i, currentWord.toUpperCase());
+                }
+            }
+        }
+
+        /** Returns the index at which the minimum value of all integers in
+         *  intList appears; if the minimum value appears more than once in
+         *  the arraylist, return the index of the first occurrence
+         *
+         *  Does NOT mutate (modify) elements in intList.
+         *  PRECONDITION: intList.size() > 0
+         *
+         *  @param intList  original arraylist of Integers
+         *  @return  the index at which the minimum value occurs
+         */
+        public static int indexOfMinimum(ArrayList<Integer> intList)
+        {
+            int minimumValue = intList.get(0);
+            int minimumIndex = 0;
+            for (int i = 0; i < intList.size(); i++)
+            {
+                if (intList.get(i) < minimumValue)
+                {
+                    minimumValue = intList.get(i);
+                    minimumIndex = i;
+                }
+            }
+            return minimumIndex;
+        }
+
+        /** Returns true if two array lists of the same length contain the exact
+         *  same elements in the same order (i.e. the two arraylists are identical).
+         *  Returns false otherwise.
+         *
+         *  Does NOT mutate (modify) elements in either arraylist
+         *  PRECONDITION: numList1.size() == numList2.size()
+         *
+         *  @param numList1  first arraylist of Integers
+         *  @param numList2  second arraylist of Integers, has the same size a first
+         *  @return  true if both arraylists are identical, element for element
+         */
+        public static boolean areIdentical(ArrayList<Integer> numList1, ArrayList<Integer> numList2)
+        {
+            int counter = 0;
+            for (int i = 0; i < numList1.size(); i++)
+            {
+                if (numList1.get(i) == numList2.get(i))
+                {
+                    counter++;
+                }
+            }
+            if (counter == numList1.size())
+            {
+                return true;
+            }
+            return false;
+        }
+
+        /** Removes all elements from numList that are ODD Integers.
+         *
+         *  DOES mutate (modify) elements in numList
+         *  PRECONDITION: numList1.size() > 0
+         *
+         *  @param numList1  arraylist of Integers
+         */
+        public static void removeOdds(ArrayList<Integer> numList)
+        {
+            for(int i = 0; i < numList.size(); i++)
+            {
+                if (numList.get(i)%2 == 1)
+                {
+                    numList.remove(i);
+                    i--;
+                }
+            }
+        }
+
+        /** Removes all elements from wordList that contain an a, e, i , and/or o.
+         *  All other words (i.e. those that have u and/or y as the vowel
+         *  such as "ugh" or "sly", or no vowels, like "psh"), add a duplicate of
+         *  that element to wordList at an adjacent index.
+         *
+         *  Assume all words have lowercase letters (i.e. no need to check for case)
+         *
+         *  DOES mutate (modify) elements in wordList
+         *  PRECONDITION: wordList.size() > 0
+         *
+         *  @param wordList  arraylist of Strings
+         */
+        public static void wackyVowels(ArrayList<String> wordList)
+        {
+            for (int i = 0; i < wordList.size(); i++)
+            {
+                if (wordList.get(i).indexOf("a") != -1 ||
+                    wordList.get(i).indexOf("e") != -1 ||
+                    wordList.get(i).indexOf("i") != -1 ||
+                    wordList.get(i).indexOf("o") != -1)
+                {
+                    wordList.remove(i);
+                    i--;
+                }
+                else
+                {
+                    String word = wordList.get(i);
+                    wordList.add(i, word);
+                    i++;
+                }
+            }
+        }
+
+        /** Removes all duplicate values from an intList, leaving only the first
+         *  occurrence in the arrayList; for example, this method will modify
+         *  [1, 2, 5, 4, 2, 2, 1, 6, 4, 4, 8, 1, 7, 4, 2] --> [1, 2, 5, 4, 6, 8, 7]
+         *
+         *  DOES mutate (modify) elements in intList
+         *  PRECONDITION: intList.size() > 0
+         *
+         *  @param intList  intList of Integers
+         */
+        public static void removeDuplicates(ArrayList<Integer> intList)
+        {
+            for (int i = 0; i < intList.size(); i++)
+            {
+                for (int j = i + 1; j < intList.size(); j++)
+                {
+                    if (intList.get(i) == intList.get(j))
+                    {
+                        intList.remove(j);
+                        j--;
+                    }
+                }
+            }
+        }
+
+        /** Adds an uppercase version of each string directly AFTER the string
+         *  in wordList; for example, if wordList is ["hello", "my", "best", "friend"]
+         *  this this method modifies wordList to be:
+         *  ["hello", "HELLO", "my", "MY", "best", "BEST", "friend", "FRIEND"]
+         *
+         *  Assume all words have lowercase letters originally
+         *
+         *  DOES mutate (modify) elements in wordList
+         *  PRECONDITION: wordList.size() > 0
+         *
+         *  @param wordList  arraylist of Strings
+         */
+        public static void duplicateUpperAfter(ArrayList<String> wordList)
+        {
+            for (int i = 0; i < wordList.size(); i++)
+            {
+                wordList.add(i + 1, wordList.get(i).toUpperCase());
+                i++;
+            }
+        }
+
+        /** Returns an arraylist of Strings that represents the input sentence parsed
+         *  into separate words (using a space: " " as the delimiter) and REVERSED
+         *  For example, if sentence = "This is my sentence!"
+         *  this method return [sentence!, my, is, This]
+         *
+         *  PRECONDITION: sentence does not end with a space
+         *
+         *  @param sentence  the input String that represents one or more words
+        separated by spaces
+         *  @return  new arraylist of Strings containing the words of sentence reversed
+         */
+        public static ArrayList<String> parseWordsAndReverse(String sentence)
+        {
+            ArrayList<String> splitList = new ArrayList<>();
+            String newSentence = sentence;
+            int index = 0;
+            while (newSentence.indexOf(" ") != -1)
+            {
+                index = newSentence.indexOf(" ");
+                splitList.add(0, newSentence.substring(0, index));
+                newSentence = newSentence.substring(index);
+            }
+            splitList.add(0, newSentence);
+            return splitList;
+        }
+
+        /** Removes all words from wordList that begin with "b" and inserts them at the
+         *  front of wordList; all "b" words that are moved should appear in the same order
+         *  in the modified arrayList as they did before being moved
+         *
+         *  For example, this method will take a wordList:
+         *  ["apple", "banana", "cherry", "donut", "bagel", "danish", "berry", "baguette", "soda"]
+         *  and modify it to
+         *  ["banana", "bagel", "berry", "baguette", "apple", "cherry", "donut", "danish", "soda"]
+         *
+         *  DOES mutate (modify) elements in wordList
+         *  PRECONDITIONS: wordList.size() > 0, all strings in wordList have at least one character
+         *
+         *  @param wordList  arraylist of words
+         */
+        /*public static void moveBWords(ArrayList<String> wordList)
+        {
+            ArrayList<String> bList = new ArrayList<>();
+            for (String str : wordList)
+            {
+                if (str.substring(0, 1).equals("b"))
+                {
+                    bList.add(str);
+                }
+            }
+            for (int i = bList.size() - 1; i >= 0; i--)
+            {
+                wordList.add(0, bList.get(i));
+            }
+        }
+        */
+
+
+
+
+    }
